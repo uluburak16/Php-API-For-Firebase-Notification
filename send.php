d1c444596a15cc717f462c84f963c5c5dfa7b41f
burakulu <uluburak16@gmail.com>
2019-04-20 14:03:39 +0300

    Update

--- send.php
+++ send.php
@@ -7,7 +7,7 @@ require_once 'src/FirebaseInitialize.php';
 $notify = new CreateNotificationProperties();
 $notify->setMessage('Test')
 	   ->setTitle('Firebase Notification')
-	   ->setDeviceToken('fywc9UQX--c:APA91bG8qCixyAJfeXiDG8Fxqz3V1BdhkFKIv0iFSQNvs_EjjBUT6fLf4eSslnwskYXRKs7GNhr0giV5Vbpo45EN9L7raAhHpx81cGqK5oGTJOz_FkBbdRDiD38UU946BznvmglT7vGj')
+	   ->setDeviceToken() //Set firebase token or tokens
 	   ->setSound(1)
 	   ->setVibrate(1);
 
